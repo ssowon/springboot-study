58304ad7a8cb9a2db7fa0879582a766f812f242b
ssowon <thdnjs5018@naver.com>
2021-02-26 19:43:37 +0900

    CRUD 완성!

--- src/main/java/com/sowon/springboot/service/posts/PostsService.java
+++ src/main/java/com/sowon/springboot/service/posts/PostsService.java
@@ -1,12 +1,16 @@
 package com.sowon.springboot.service.posts;
 import com.sowon.springboot.domain.posts.Posts;
 import com.sowon.springboot.domain.posts.PostsRepository;
+import com.sowon.springboot.web.dto.PostsListResponseDto;
 import com.sowon.springboot.web.dto.PostsResponseDto;
 import com.sowon.springboot.web.dto.PostsSaveRequestDto;
 import com.sowon.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() ->new IllegalArgumentException("해당 게시글이 없습니다. id= "+ id));
+
+        postsRepository.delete(posts);
     }
 }
