220f3d9ba0294729584a45b91cee5ee59cd26f5d
ssowon <thdnjs5018@naver.com>
2021-02-27 16:46:00 +0900

    OAuth 네이버 로그인 구현

--- src/main/java/com/sowon/springboot/web/IndexController.java
+++ src/main/java/com/sowon/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sowon.springboot.web;
 
+import com.sowon.springboot.config.auth.LoginUser;
 import com.sowon.springboot.config.auth.dto.SessionUser;
 import com.sowon.springboot.service.posts.PostsService;
 import com.sowon.springboot.web.dto.PostsResponseDto;
@@ -19,11 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-        if(user != null) {
+        if (user != null) {
             model.addAttribute("userName", user.getName());
         }
         return "index";
